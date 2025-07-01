064492f3377dd234a1245a570560ec78e2de1f61
Michaelrobins938 <99077456+Michaelrobins938@users.noreply.github.com>
2025-06-30 22:18:15 -0500

    Update debug.js

--- api/debug.js
+++ api/debug.js
@@ -1,13 +1,16 @@
-import { GoogleSpreadsheet } from 'google-spreadsheet';
-
+// /api/debug.js
 export default function handler(req, res) {
   try {
+    // Don't try to access package.json directly - use alternative methods
+    const { GoogleSpreadsheet } = require('google-spreadsheet');
+    
     const hasEnvVar = !!process.env.SERVICE_ACCOUNT_KEY;
     const envVarLength = process.env.SERVICE_ACCOUNT_KEY?.length || 0;
-
+    
+    // Test if GoogleSpreadsheet constructor works
     let constructorWorks = false;
     let hasUseServiceAccountAuth = false;
-
+    
     try {
       const testDoc = new GoogleSpreadsheet('test-id');
       constructorWorks = true;
@@ -15,13 +18,14 @@ export default function handler(req, res) {
     } catch (e) {
       constructorWorks = false;
     }
-
+    
     res.json({
       constructorWorks,
       hasUseServiceAccountAuth,
       hasServiceAccountKey: hasEnvVar,
       serviceAccountKeyLength: envVarLength,
       nodeVersion: process.version,
+      // Try to parse env var
       serviceAccountKeyValid: hasEnvVar ? (() => {
         try {
           JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
@@ -32,6 +36,9 @@ export default function handler(req, res) {
       })() : false
     });
   } catch (error) {
-    res.json({ error: error.message, stack: error.stack });
+    res.json({ 
+      error: error.message,
+      stack: error.stack 
+    });
   }
 }
