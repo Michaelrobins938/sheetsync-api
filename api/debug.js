64dcd457a9a1c9b5cb5b62ba839cd69331c44fd6
Michaelrobins938 <99077456+Michaelrobins938@users.noreply.github.com>
2025-06-30 21:49:54 -0500

    Update debug.js

--- api/debug.js
+++ api/debug.js
@@ -1,17 +1,37 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet';
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   try {
-    const pkg = require('google-spreadsheet/package.json');
-    const dummyDoc = new GoogleSpreadsheet("DUMMY_ID");
+    const hasEnvVar = !!process.env.SERVICE_ACCOUNT_KEY;
+    const envVarLength = process.env.SERVICE_ACCOUNT_KEY?.length || 0;
 
-    res.status(200).json({
-      googleSpreadsheetVersion: pkg.version,
-      useServiceAccountAuthExists: typeof dummyDoc.useServiceAccountAuth === 'function',
-      hasServiceAccountKey: !!process.env.SERVICE_ACCOUNT_KEY,
-      nodeVersion: process.version
+    let constructorWorks = false;
+    let hasUseServiceAccountAuth = false;
+
+    try {
+      const testDoc = new GoogleSpreadsheet('test-id');
+      constructorWorks = true;
+      hasUseServiceAccountAuth = typeof testDoc.useServiceAccountAuth === 'function';
+    } catch (e) {
+      constructorWorks = false;
+    }
+
+    res.json({
+      constructorWorks,
+      hasUseServiceAccountAuth,
+      hasServiceAccountKey: hasEnvVar,
+      serviceAccountKeyLength: envVarLength,
+      nodeVersion: process.version,
+      serviceAccountKeyValid: hasEnvVar ? (() => {
+        try {
+          JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+          return true;
+        } catch {
+          return false;
+        }
+      })() : false
     });
   } catch (error) {
-    res.status(500).json({ error: error.message, stack: error.stack });
+    res.json({ error: error.message, stack: error.stack });
   }
 }
